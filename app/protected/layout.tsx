0a5dda03b8ed66350da468730950db885aad3827
Anton Grabovsky <agrabovsky2020@gmail.com>
2025-08-26 11:36:12 +0300

    feat: Change UI

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,4 +1,3 @@
-import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -16,10 +15,8 @@ export default function ProtectedLayout({
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
+              <Link href={"/"}>TodoList</Link>
+              <div className="flex items-center gap-2"></div>
             </div>
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
@@ -30,15 +27,8 @@ export default function ProtectedLayout({
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
+            © {new Date().getFullYear()}&nbsp;
+            <a href="https://github.com/sk1t0n">sk1t0n</a>. All rights reserved.
           </p>
           <ThemeSwitcher />
         </footer>
